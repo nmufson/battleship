ef2918407d0f0eebe6febe58d3d8d118dcb84506
Nick <nickmufson1@gmail.com>
2024-05-24 17:33:07 -0400

    add prettier eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,33 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import js from '@eslint/js';
-
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import js from "@eslint/js";
 
 export default [
   js.configs.recommended,
-  {files: ['src/**/*.js'], languageOptions: {sourceType: 'commonjs'}}, 
+  { files: ["src/**/*.js"], languageOptions: { sourceType: "commonjs" } },
   {
-    'rules': {
-      'eqeqeq': 'error',
-      'curly': 'error',
-      'semi': ['error', 'always'],
-      'no-unused-vars': ['warn', { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false }],
-      'indent': ['error', 2],
-      'no-console': 'warn',
-      'arrow-spacing': ['error', { 'before': true, 'after': true }],
-      'no-var': 'error',
-      'prefer-const': 'error',
-      'quotes': [
-        'error',
-        'single'
-      ]
-    }
+    extends: {
+      eslint: "recommended",
+      plugin: "prettier/recommended",
+    },
+    plugins: ["prettier"],
+    rules: {
+      prettier: "error",
+      eqeqeq: "error",
+      curly: "error",
+      semi: ["error", "always"],
+      "no-unused-vars": [
+        "warn",
+        { vars: "all", args: "after-used", ignoreRestSiblings: false },
+      ],
+      indent: ["error", 2],
+      "no-console": "warn",
+      "arrow-spacing": ["error", { before: true, after: true }],
+      "no-var": "error",
+      "prefer-const": "error",
+      quotes: ["error", "single"],
+    },
   },
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
-
